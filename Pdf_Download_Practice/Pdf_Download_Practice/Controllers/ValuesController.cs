2b0d53b463c6bb75fab76a6d11ecefc22681fcd0
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-08-28 21:18:32 +0200

    Initial commit

---
+++ Pdf_Download_Practice/Pdf_Download_Practice/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Pdf_Download_Practice.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        [HttpGet("getpdf")]
+        public IActionResult GetFile()
+        {
+            var stream = new FileStream(@"C:\Users\laszl\Documents\Programozás\greenfox\pdf_download_endpoint_practice\Pdf_Download_Practice\Pdf_Download_Practice\Files\etlap.pdf", FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+        }
+    }
+}
